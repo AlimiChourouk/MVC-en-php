b8882d42a496d38b230506c074eb65cea62746ad
AlimiChourouk <Alimi.Chourouk.solicode@gmail.com>
2025-03-11 02:44:31 +0000

    Add files via upload

---
+++ MVC/controllers/RestaurantController.php
@@ -0,0 +1,15 @@
+<?php
+require_once(__DIR__ .'/../models/PlatModel.php');
+
+        function afficherPlats() {
+            $plats = getAllPlats();
+            $platsParCuisine = [];
+            foreach ($plats as $plat) {
+                $platsParCuisine[$plat['TypeCuisine']][] = $plat;
+            }
+            require (__DIR__ .'/../views/restaurantsView.php'); 
+        }
+       
+       
+       
+    ?>
---
+++ MVC/models/PlatModel.php
@@ -0,0 +1,11 @@
+<?php
+require_once (__DIR__ .'/../config/db.php');
+
+function getAllPlats() {
+    global $pdo;
+    $sql = "SELECT * FROM plat";
+    $stmt = $pdo->query($sql);
+    return $stmt->fetchAll();
+  
+}
+?>
